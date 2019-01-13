12e81dba182f61042414b4d771ad9cf8ed154d69
Sidenko_Oleksandr <sidoj@meta.ua>
2019-01-13 11:01:00 +0200

    corrected edit task

--- home_13/src/js/main.js
+++ home_13/src/js/main.js
@@ -16,34 +16,32 @@ $('.buttnSave').click(function () {
   if (!valueTask) {                           //empty task input validation
     alert('Please, enter your new task!')
   } else {
-    $('.addedTask').prepend('<li class="border rounded itemTask">'+'<p class="textTask">'+valueTask+'</p>'+ // send new task to the actual list
-      '<div class="d-inline">'+
-      '<button class="btn btn-outline-secondary buttnEdit">Edit</button>'+
-      '<button class="btn btn-outline-danger buttnDel">Del</button>'+'</div>'+
+    $('.addedTask').prepend('<li class="border rounded itemTask">' +
+      '<p class="textTask">' + valueTask + '</p>' + // send new task to the actual list
+      '<div class="d-inline">' +
+      '<button class="btn btn-outline-secondary buttnEdit">Edit</button>' +
+      '<button class="btn btn-outline-danger buttnDel">Del</button>' + '</div>' +
       '</li>')
-
     $('.form-control').val('')                            // clear input
   }
 })
 
 //jquery function click-listener button Clear
 $('.buttnClear').click(function () {
-  valueTask =''
+  valueTask = ''
   $('.form-control').val(valueTask)
 })
 
 //jquery function click-listener button Edit to correct saved task
-$('body').on('click', '.buttnEdit', function() {
-  if (confirm('Correct this task')){
-    $('.textTask').attr('contenteditable', true)
+$('body').on('click', '.buttnEdit', function () {
+  if (confirm('Correct this task')) {
+    $(this).parents('.itemTask').children('p').attr('contenteditable', true)
   }
 })
 
 //jquery function click-listener button Del,  using .on() to delegate click event to exist element - body
-$('body').on('click', '.buttnDel', function() {
-  if (confirm('You want remove this task from list! Are you sure?')){
+$('body').on('click', '.buttnDel', function () {
+  if (confirm('You want remove this task from list! Are you sure?')) {
     $(this).parents('.itemTask').remove()
   }
 })
-
-
