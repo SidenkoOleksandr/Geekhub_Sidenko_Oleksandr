c1e532fef51457d24e138714b1669a22ad5c8a7d
Sidenko_Oleksandr <sidoj@meta.ua>
2019-01-13 09:44:58 +0200

    add edit task

--- home_13/src/js/main.js
+++ home_13/src/js/main.js
@@ -16,8 +16,9 @@ $('.buttnSave').click(function () {
   if (!valueTask) {                           //empty task input validation
     alert('Please, enter your new task!')
   } else {
-    $('.addedTask').prepend('<li class="border rounded itemTask">'+'<p>'+valueTask+'</p>'+ // send new task to the actual list
+    $('.addedTask').prepend('<li class="border rounded itemTask">'+'<p class="textTask">'+valueTask+'</p>'+ // send new task to the actual list
       '<div class="d-inline">'+
+      '<button class="btn btn-outline-secondary buttnEdit">Edit</button>'+
       '<button class="btn btn-outline-danger buttnDel">Del</button>'+'</div>'+
       '</li>')
 
@@ -31,12 +32,18 @@ $('.buttnClear').click(function () {
   $('.form-control').val(valueTask)
 })
 
+//jquery function click-listener button Edit to correct saved task
+$('body').on('click', '.buttnEdit', function() {
+  if (confirm('Correct this task')){
+    $('.textTask').attr('contenteditable', true)
+  }
+})
+
 //jquery function click-listener button Del,  using .on() to delegate click event to exist element - body
 $('body').on('click', '.buttnDel', function() {
   if (confirm('You want remove this task from list! Are you sure?')){
     $(this).parents('.itemTask').remove()
   }
 })
-
 
 
